b1f59397b49c79f681739f39503bafa365eb07b9
Smoker Jones <smokerjones@pm.me>
2020-12-21 17:42:34 +0100

    added NER/RE examples

--- EntityExtractor.py
+++ EntityExtractor.py
@@ -2,3 +2,43 @@
 
 
 #class EntityExtractor
+
+
+from transformers import AutoModelForTokenClassification, AutoTokenizer, pipeline
+import torch
+
+
+nlp = pipeline("ner")
+
+sequence = "Hugging Face Inc. is a company based in New York City. Its headquarters are in DUMBO, therefore very" \
+           "close to the Manhattan Bridge which is visible from the window."
+
+print(nlp(sequence))
+
+
+# model = AutoModelForTokenClassification.from_pretrained("dbmdz/bert-large-cased-finetuned-conll03-english")
+# tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+# label_list = [
+#     "O",       # Outside of a named entity
+#     "B-MISC",  # Beginning of a miscellaneous entity right after another miscellaneous entity
+#     "I-MISC",  # Miscellaneous entity
+#     "B-PER",   # Beginning of a person's name right after another person's name
+#     "I-PER",   # Person's name
+#     "B-ORG",   # Beginning of an organisation right after another organisation
+#     "I-ORG",   # Organisation
+#     "B-LOC",   # Beginning of a location right after another location
+#     "I-LOC"    # Location
+# ]
+
+# sequence = "Hugging Face Inc. is a company based in New York City. Its headquarters are in DUMBO, therefore very" \
+#            "close to the Manhattan Bridge."
+
+# # Bit of a hack to get the tokens with the special tokens
+# tokens = tokenizer.tokenize(tokenizer.decode(tokenizer.encode(sequence)))
+# inputs = tokenizer.encode(sequence, return_tensors="pt")
+
+# outputs = model(inputs)[0]
+# predictions = torch.argmax(outputs, dim=2)
+
+# print([(token, label_list[prediction]) for token, prediction in zip(tokens, predictions[0].tolist())])
--- main.py
+++ main.py
@@ -22,12 +22,21 @@ while(text_provider.has_next()):
     #filter out empty string list items
     sentences = list(filter(lambda x: x.strip() != '', sentences))  
 
-#len(test_string.split()) 
+    #since I require 2 entities to predict relations, I should ignore sentences less than 3 words in length
+    #len(test_string.split()) 
 
-    for sentence in sentences:        
+    for sentence in sentences:  
+        #ignore sentences with length less than 3 words
+        if len(sentence.split()) < 3:
+            continue
+
         print(sentence)
 
+
     #identify entities
+    #should I take the entities in a sentence with the greatest accuracy confidence?
+
+
 
 
     #identify relations between entities
